0b7c44f57ddf3c433d8e3adb8ab9b54585c91e41
Matthew Kaiser <MatthewKaiser45@gmail.com>
2021-02-10 13:11:20 -0500

    Test adding multiple PIDs to the stream

--- test/test_CAN_Sniffer Add_PID.c
+++ test/test_CAN_Sniffer Add_PID.c
@@ -47,3 +47,22 @@ void test_add_turbo_inlet_pressure_stream(void)
     TEST_ASSERT_EQUAL( MODE1_TURBO_INLET_PRESSURE , sniffer.stream[0]->pid );
     TEST_ASSERT_EQUAL( MODE1 , sniffer.stream[0]->mode );
 }
+
+void test_add_multiple_to_stream(void)
+{
+    PID_SUPPORTED_STATUS status = 0xFF;
+    PID_DATA pid_req = { .pid = MODE1_ENGINE_OIL_TEMPERATURE, .mode = MODE1, .pid_unit = PID_UNITS_CELCIUS };
+    status = CAN_Sniffer_Add_PID( &sniffer, &pid_req );
+    TEST_ASSERT_EQUAL( PID_SUPPORTED , status );
+    TEST_ASSERT_EQUAL( MODE1_ENGINE_OIL_TEMPERATURE , sniffer.stream[0]->pid );
+    TEST_ASSERT_EQUAL( MODE1 , sniffer.stream[0]->mode );
+
+    status = 0xFF;
+    pid_req.pid = MODE1_TURBO_INLET_PRESSURE;
+    pid_req.mode = MODE1;
+    pid_req.pid_unit = PID_UNITS_KPA;
+    status = CAN_Sniffer_Add_PID( &sniffer, &pid_req );
+    TEST_ASSERT_EQUAL( PID_SUPPORTED , status );
+    TEST_ASSERT_EQUAL( MODE1_TURBO_INLET_PRESSURE , sniffer.stream[1]->pid );
+    TEST_ASSERT_EQUAL( MODE1 , sniffer.stream[1]->mode );
+}
