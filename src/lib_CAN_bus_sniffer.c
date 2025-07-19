ab03904a29df0911c3bb96597e753122bfa8715d
Matt Kaiser <MatthewKaiser45@gmail.com>
2025-07-19 14:08:16 -0400

    Correct typo

--- src/lib_CAN_bus_sniffer.c
+++ src/lib_CAN_bus_sniffer.c
@@ -485,13 +485,13 @@ void CAN_Sniffer_Add_Packet( PCAN_SNIFFER_PACKET_MANAGER dev, uint16_t arbitrati
                     defined(SNIFF_CRUISE_CONTROL_CAN_BUTTON_PID)
                 case 0x030:
 
-                    /* Cruise Control OFF button Status */
+                    /* Cruise Control ON button Status */
                     if( dev->stream[i]->pid_uuid == SNIFF_CRUISE_CONTROL_ON_BUTTON_UUID ) {
                         dev->stream[i]->pid_value = (float)((data[5] & 0x01) > 0);
                         process_change(dev->stream[i]);
                     }
 
-                    /* Cruise Control ON Button Status */
+                    /* Cruise Control OFF Button Status */
                     else if( dev->stream[i]->pid_uuid == SNIFF_CRUISE_CONTROL_OFF_BUTTON_UUID ) {
                         dev->stream[i]->pid_value = (float)((data[5] & 0x02) > 0);
                         process_change(dev->stream[i]);
